0bf0e5129a0c04e54b03b9e7da0976c7ce628c1b
York Ng <yorkhayng@gmail.com>
2025-07-31 21:56:01 -0400

    support parsing for all legco records (incl. colonial)

---
+++ parse.py
@@ -0,0 +1,100 @@
+from pdfminer.high_level import extract_text
+import html2text
+import re
+import pandas as pd
+from argparse import ArgumentParser
+from striprtf.striprtf import rtf_to_text
+import textract
+
+name_regex = r"^(?!MEMBER|CLERKS|CLERK IN|PUBLIC OFFICER)\s*[A-Z][A-Z\s,\'-]*(?:asked)?:"
+
+def remove_lines_regex(text, pattern):
+    if not text:
+        return ''
+    lines = text.split('\n')
+    filtered_lines = [line for line in lines if not re.search(pattern, line)]
+    return '\n'.join(filtered_lines)
+
+
+def remove_after_keyword(text, keyword):
+    if not text:
+        return ''
+    lines = text.split('\n')
+    for i in range(len(lines) - 1, -1, -1):
+        if keyword in lines[i]:
+            return '\n'.join(lines[:i + 1])
+    return text
+
+
+def remove_lines_before_regex(text, patterns):
+    # Remove all lines before the first occurrence of any pattern
+    if not text:
+        return ''
+    lines = text.split('\n')
+    for i in range(len(lines)):
+        if any(re.search(pattern, lines[i]) for pattern in patterns):
+            return '\n'.join(lines[i:])
+
+
+def parse(filename):
+    file_type = filename.split('.')[-1].lower()
+    if file_type == 'pdf':
+        text = extract_text(filename)
+    elif file_type == 'htm':
+        h = html2text.HTML2Text()
+        with open(filename, 'r', encoding='utf-8') as f:
+            html_content = f.read()
+            text = h.handle(html_content)
+    else:
+        with open(filename, 'r', encoding='utf-8') as f:
+            try:
+                text = textract.process(filename, encoding='utf-8', errors='ignore').decode('utf-8')
+            except:
+                content = f.read()
+                text = rtf_to_text(content, errors='ignore')
+
+    text = re.sub(r'[\x00-\x08\x0b\x0c\x0e-\x1f\x7f]', '', text)
+    text = re.sub(r'[\*_|]', '', text)
+    text = re.sub(r' +', ' ', text)
+    text = re.sub(r'^\d\\*.\s+', '\n', text, flags=re.MULTILINE)
+    text = remove_lines_before_regex(text, [r"(in Cantonese)", r"(in English)", r"(in Chinese)", name_regex])
+    for phrase in ('Suspended accordingly', 'Adjourned accordingly'):
+        text = remove_after_keyword(text, phrase)
+        text = remove_lines_regex(text, phrase)
+    text = remove_lines_regex(text, r'LEGISLATIVE COUNCIL')
+    text = remove_lines_regex(text, r'^\d+\s*$')
+    text = re.sub(r'\(.*?\)', '', text, flags=re.DOTALL)
+    text = re.sub(r'(?:\n[ ]*){3,}', '\n\n', text)
+
+    # Attribute speakers to their "speeches"
+    results = {'speaker': [], 'text': []}
+    lines = text.split('\n')
+    current_speaker = None
+    current_text = ''
+    for line in lines:
+        if line.strip():  # Check if the line is not empty
+            if re.match(name_regex, line):
+                if current_speaker is not None:
+                    results['speaker'].append(current_speaker)
+                    results['text'].append(current_text.strip())
+                current_speaker = line.split(':')[0].strip()
+                current_speaker = current_speaker.replace('asked', '').strip()
+                current_text = line.split(':')[1].strip() + ' '
+            else:
+                current_text += line.strip() + ' '
+    if current_speaker is not None:
+        results['speaker'].append(current_speaker)
+        results['text'].append(current_text.strip())
+
+    df = pd.DataFrame(results)
+    return df
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="Parse a Legislative Council transcript PDF.")
+    parser.add_argument('--filename', type=str, help='Path to the PDF file to parse.')
+    parser.add_argument('--output', type=str, default='parsed_speech.csv', help='Output CSV file name.')
+    args = parser.parse_args()
+
+    df = parse(args.filename)
+    df.to_csv(args.output, index=False)
--- scrape_pdfs.py
+++ scrape_pdfs.py
@@ -3,15 +3,46 @@ from datetime import datetime, timedelta
 import time
 
 
-def generate_legco_urls(start_year: int = 2014, end_year: int = 2024) -> list[tuple[str, str, str]]:
+def generate_legco_urls(start_year: int = 2014, end_year: int = 2024) -> list[tuple[str, str, int, str]]:
     """
     Generate URLs for the Legislative Council (LegCo) 'Official Record of Proceedings' from 1998 to 2024.
     The URLs directly link to files (e.g. pdf, htm, doc, etc.).
     Currently, only records from the 1998 legislative year onwards is supported.
     :param start_year: The first legislative year to generate URLs for.
     :param end_year: The last legislative year to generate URLs for.
-    :return: A list of tuples containing the legislative year, date of the meeting in 'yymmdd' format, and the URL.
+    :return: A list of tuples containing the legislative year, date of the meeting in 'yymmdd' format, document index, and the URL.
     """
+    parts = {
+        'en': {
+            datetime(2000, 6, 27): [f'https://www.legco.gov.hk/yr99-00/english/counmtg/hansard/000627f{part}.pdf' for part in ('a', 'b')],
+            datetime(2000, 5, 24): [f'https://www.legco.gov.hk/yr99-00/english/counmtg/hansard/000524{part}.pdf' for part in ('fe', 't')],
+            datetime(1999, 12, 2): ['https://www.legco.gov.hk/yr99-00/english/counmtg/hansard/991202fb.pdf'],
+            datetime(1999, 7, 16): [f'https://www.legco.gov.hk/yr98-99/english/counmtg/hansard/99716fe{part}.pdf' for
+                                    part in ('1', '2')],
+            datetime(1999, 7, 15): [f'https://www.legco.gov.hk/yr98-99/english/counmtg/hansard/99715fe{part}.pdf' for
+                                    part in ('1', '2')],
+            datetime(1999, 7, 14): [f'https://www.legco.gov.hk/yr98-99/english/counmtg/hansard/99714fe{part}.pdf' for part in ('1', '2')],
+            datetime(1999, 3, 10): [f'https://www.legco.gov.hk/yr98-99/english/counmtg/hansard/990310f{part}.htm' for part in ('a', 'b', 'c', 'd')],
+            datetime(1997, 9, 27): [f'counmtg/hansard/970927{part}.htm' for part in ('a', 'b')],
+            datetime(1998, 2, 25): [f'counmtg/hansard/980225{part}.doc' for part in ('a', 'b')],
+            # thankfully none of the 96-97 legco meetings conflict with 97 provisional legco
+            datetime(1997, 6, 27): [f'lc_sitg/hansard/970627f{part}.doc' for part in ('a', 'b', 'c')],
+            datetime(1997, 6, 26): [f'lc_sitg/hansard/970626f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 6, 25): [f'lc_sitg/hansard/970625f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 6, 24): [f'lc_sitg/hansard/970624f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 6, 23): [f'lc_sitg/hansard/970623f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 6, 17): [f'lc_sitg/hansard/970617f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 6, 11): [f'lc_sitg/hansard/970611f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 6, 4): [f'lc_sitg/hansard/970604f{part}.doc' for part in ('a', 'b')],
+            datetime(1997, 5, 28): [f'lc_sitg/hansard/970528f{part}.doc' for part in ('a', 'b', 'c', 'd')],
+            datetime(1997, 4, 9): [f'lc_sitg/hansard/970409f{part}.doc' for part in ('a', 'b')],
+            datetime(1996, 7, 28): ['lc_sitg/hansard/h950728.pdf'],
+            datetime(1996, 5, 29): ['lc_sitg/hansard/han2905.htm'],
+            datetime(1996, 5, 15): ['lc_sitg/hansard/han1505.htm'],
+            datetime(1996, 5, 1): ['lc_sitg/hansard/han0105.htm'],
+            datetime(1996, 2, 7): [f'lc_sitg/hansard/960207{part}.doc' for part in ('fe', 't')]
+        }
+    }
     urls = []
 
     for year1 in range(start_year, end_year + 1):
@@ -22,7 +53,19 @@ def generate_legco_urls(start_year: int = 2014, end_year: int = 2024) -> list[tu
             # from 2022 onwards
             legco_year = f"yr{str(year1)}"
 
-        if year1 == 1998:
+        if year1 == 1983:
+            start_date = datetime(year1, 9, 27)
+            end_date = datetime(year2, 9, 26)
+        elif year1 < 1985:
+            start_date = datetime(year1, 10, 1)
+            end_date = datetime(year2, 8, 16) # account for 1977, 1979, 1982, 1985, etc.
+        elif year1 < 1997:
+            start_date = datetime(year1, 10, 1)
+            end_date = datetime(year2, 7, 31)
+        elif year1 == 1997: # provisional council
+            start_date = datetime(year1, 2, 22)
+            end_date = datetime(year2, 4, 8)
+        elif year1 == 1998:
             start_date = datetime(year1, 7, 1)
             end_date = datetime(year2, 7, 31)
         elif year1 < 2022:
@@ -35,47 +78,69 @@ def generate_legco_urls(start_year: int = 2014, end_year: int = 2024) -> list[tu
             end_date = datetime(year1, 12, 31)
         current_date = start_date
 
+        base_url = f'https://www.legco.gov.hk/{legco_year}/english/'
+
         while current_date <= end_date:
-            if current_date.weekday() < 5:  # Weekdays only
-                if year1 < 1998:
-                    continue
-                if current_date < datetime(1999, 6, 16):
+            if (year1 in (1996, 1997)) or current_date.weekday() < 5:
+                # check only weekdays, except for some years
+                yyyymmdd = current_date.strftime("%Y%m%d")
+                yymmdd = current_date.strftime("%y%m%d")
+                mmdd = current_date.strftime("%m%d")
+                ddmm = current_date.strftime("%d%m")
+
+                if current_date in parts['en']:
+                    files = parts['en'][current_date]
+                elif year1 < 1995:
+                    files = [f'lc_sitg/hansard/h{yymmdd}.pdf']
+                elif current_date <= datetime(1995, 12, 6):
+                    files = [f'lc_sitg/hansard/han{ddmm}.htm']
+                elif current_date <= datetime(1996, 7, 10):
+                    files = [f'lc_sitg/hansard/{yymmdd}fe.doc']
+                elif current_date <= datetime(1996, 10, 9):
+                    files = [f'lc_sitg/hansard/han{ddmm}.htm']
+                elif year1 == 1996:
+                    # 96-97 colonial legco, after 10th oct 1996
+                    files = [f"lc_sitg/hansard/{yymmdd}fe.doc"]
+                elif year1 == 1997 and current_date <= datetime(1997, 10, 10):
+                    # provision legco, before 10th oct 1997
+                    files = [f"counmtg/hansard/{yymmdd}fe.htm"]
+                elif year1 == 1997:
+                    # provisional legco, after 10th oct 1997
+                    files = [f"counmtg/hansard/{yymmdd}fe.doc"]
+                elif current_date < datetime(1999, 6, 16):
                     # in the 1st legco before 16th June 1999, they are stored in HTM
-                    yymmdd = current_date.strftime("%y%m%d")
-                    url = f"https://www.legco.gov.hk/{legco_year}/english/counmtg/hansard/{yymmdd}fe.htm"
+                    files = [f"counmtg/hansard/{yymmdd}fe.htm"]
                 elif current_date < datetime(2001, 10, 17):
                     # from 16th june 1999 to 16th october 2001
-                    yymmdd = current_date.strftime("%y%m%d")
-                    url = f"https://www.legco.gov.hk/{legco_year}/english/counmtg/hansard/{yymmdd}fe.pdf"
+                    files = [f"counmtg/hansard/{yymmdd}fe.pdf"]
                 elif year1 < 2006:
                     # from 17th october 2001 to 2005
-                    mmdd = current_date.strftime("%m%d")
-                    url = f"https://www.legco.gov.hk/{legco_year}/english/counmtg/hansard/cm{mmdd}ti-translate-e.pdf"
+                    files = [f"counmtg/hansard/cm{mmdd}ti-translate-e.pdf"]
                 elif year1 < 2014:
                     # from 2006 to 2013
-                    mmdd = current_date.strftime("%m%d")
-                    url = f"https://www.legco.gov.hk/{legco_year}/english/counmtg/hansard/cm{mmdd}-translate-e.pdf"
+                    files = [f"counmtg/hansard/cm{mmdd}-translate-e.pdf"]
                 else:
                     # from yr14-15 onwards
-                    yyyymmdd = current_date.strftime("%Y%m%d")
-                    url = f"https://www.legco.gov.hk/{legco_year}/english/counmtg/hansard/cm{yyyymmdd}-translate-e.pdf"
-                urls.append((legco_year, current_date.strftime("%y%m%d"), url))
+                    files = [f"counmtg/hansard/cm{yyyymmdd}-translate-e.pdf"]
+
+                for i, file in enumerate(files):
+                    urls.append((legco_year, current_date.strftime("%y%m%d"), i, base_url + file))
             current_date += timedelta(days=1)
 
     print(f"Finished URL generation. Total generated: {len(urls)}\n")
     return urls
 
 
-def scrape_urls(urls: list[tuple[str, str, str]], sleep_time: float = 0.5) -> list[str]:
+def scrape_urls(urls: list[tuple[str, str, int, str]], sleep_time: float = 0.5) -> list[str]:
     """
     Scrape the URLs to check if they are valid and download the files.
-    :param urls: A list of tuples containing the legislative year, date of the meeting in 'yymmdd' format, and the URL.
+    :param urls: A list of tuples containing the legislative year, date of the meeting in 'yymmdd' format, document index, and the URL.
     :param sleep_time: Time to sleep between requests to avoid overwhelming the server.
     :return: A list of valid URLs that were successfully downloaded.
     """
     valid_urls = []
 
-    for i, (legco_year, yymmdd, url) in enumerate(urls):
+    for i, (legco_year, yymmdd, idx, url) in enumerate(urls):
         try:
             print(f"[{i+1}/{len(urls)}] Checking: {url}")
             response = requests.get(url, timeout=15)
@@ -84,7 +149,7 @@ def scrape_urls(urls: list[tuple[str, str, str]], sleep_time: float = 0.5) -> li
                 print(f"Found: {url}")
                 valid_urls.append(url)
                 file_extension = url.split('.')[-1]
-                name = f"{legco_year}_{yymmdd}.{file_extension}"
+                name = f"{legco_year}_{yymmdd}_{idx}.{file_extension}"
                 with open(f"files/{name}", "wb") as f:
                     f.write(response.content)
                     print(f"Saved file: {name}")
@@ -100,9 +165,9 @@ def scrape_urls(urls: list[tuple[str, str, str]], sleep_time: float = 0.5) -> li
 
 
 if __name__ == "__main__":
-    all_urls = generate_legco_urls(1998, 2005)
+    all_urls = generate_legco_urls(1985, 1989)
     existing_urls = scrape_urls(all_urls)
 
     with open("legco_pdf_urls.txt", "a") as f:
-        for year, url in existing_urls:
-            f.write(f"{year},{url}\n")
+        for url in existing_urls:
+            f.write(f"{url}\n")
